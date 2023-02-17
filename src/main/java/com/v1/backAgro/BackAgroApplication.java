5fef5130139abbf6f946f1389524aa3ee658df24
marcos <marcosep192000@hotmail.com>
2023-02-16 21:31:45 -0300

    first commit

---
+++ src/main/java/com/v1/backAgro/BackAgroApplication.java
@@ -0,0 +1,13 @@
+package com.v1.backAgro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackAgroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackAgroApplication.class, args);
+	}
+
+}
